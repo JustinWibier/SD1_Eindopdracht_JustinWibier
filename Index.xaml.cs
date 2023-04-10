a2d45c3705dfdc235b40bb692d57807c5faee59b
JustinWibier <89913177+JustinWibier@users.noreply.github.com>
2023-04-10 18:47:16 +0200

    start

--- Index.xaml.cs
+++ Index.xaml.cs
@@ -16,18 +16,13 @@ using System.Windows.Shapes;
 namespace SoftwareDevelopment1_Eindopdracht
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Page1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Page1 : Page
     {
-        public MainWindow()
+        public Page1()
         {
             InitializeComponent();
-        }
-
-        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
         }
     }
 }
