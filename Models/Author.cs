e23bc77a47a8ad85f710a76f6f83da59381363f9
JustinWibier <89913177+JustinWibier@users.noreply.github.com>
2023-04-14 02:42:13 +0200

    finished the code for the CRUD of Authors

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SoftwareDevelopment1_Eindopdracht.Models
 {
-    public class Author
+    public class Author : INotifyPropertyChanged
     {
-        public string Name { get; set; }
-        public string Lastname { get; set; }
-        public string? Infix { get; set; }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private string _name;
+        private string _lastname;
+        private string? _infix;
+
+        public string Name { get => _name;set { _name = value; Notify("Name"); }}
+        public string Lastname { get => _lastname; set { _lastname = value; Notify("Lastname"); } }
+        public string? Infix { get => _infix; set { _infix = value; Notify("Infix"); } }
         public int Author_Id { get; set; }
+
+        private void Notify(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("propertyName"));
+        }
     }
+
 }
---
+++ Viewmodels/CRUD_Authors_ViewModel.cs
@@ -0,0 +1,70 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using SoftwareDevelopment1_Eindopdracht.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SoftwareDevelopment1_Eindopdracht.Viewmodels
+{
+    public class Update_Authors_ViewModel
+    {
+        public Author SelectedAuthor { get; set; }
+        public ObservableCollection<Author> Allauthors  { get; set; }
+
+        public ICommand AddNew_Author { get; set; }
+
+        public ICommand Delete_Author { get; set; }
+
+        public Author author { get; set; }
+
+        public Update_Authors_ViewModel()
+        {
+            author = new Author
+            {
+                Name = "new",
+                Infix = "-",
+                Lastname = "new",
+            };
+
+            Allauthors = new ObservableCollection<Author>
+            {
+                new Author
+                {
+                    Name= "Yumi",
+                    Lastname= "Argento"
+                },
+                new Author
+                {
+                    Name= "Astra",
+                    Infix= "von",
+                    Lastname= "Lichtenstein",
+                }
+            };
+            AddNew_Author = new RelayCommand(Add_New_Author);
+            Delete_Author = new RelayCommand(Delete_author);
+
+        }
+
+        private void Add_New_Author()
+        {
+            Allauthors.Add(new Author
+            {
+                Name = author.Name,
+                Infix = author.Infix,
+                Lastname = author.Lastname
+            });
+        }
+
+        private void Delete_author()
+        {
+            SelectedAuthor.Name = "[DELETED]";
+            SelectedAuthor.Infix = "_";
+            SelectedAuthor.Lastname = "_";
+        }
+
+    }
+}
